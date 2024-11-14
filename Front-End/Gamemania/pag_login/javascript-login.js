1c85f431727edd5c24d4c331e606740b8d1637f6
João Lopes <vlopes289@gmail.com>
2024-11-14 14:09:18 -0300

    login working

---
+++ Front-End/Gamemania/pag_login/javascript-login.js
@@ -0,0 +1,16 @@
+const validUsername = "admin123";
+const validPassword = "senha123";
+
+function validateLogin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("result");
+
+    if (username === validUsername && password === validPassword) {
+        message.textContent = "Login bem-sucedido!";
+        message.style.color = "white";
+    } else {
+        message.textContent = "Usuário ou senha incorretos.";
+        message.style.color = "pink";
+    }
+}
