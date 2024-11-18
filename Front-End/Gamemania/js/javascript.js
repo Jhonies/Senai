319d212c90c4d8290ac4bcb63ecf064c2837871b
abedala <patrickabedala@hotmail.com>
2024-11-18 20:17:33 -0300

    updt-presencial

--- Front-End/Gamemania/js/javascript.js
+++ Front-End/Gamemania/js/javascript.js
@@ -8,3 +8,23 @@ function moveCarrossel() {
     const offset = -currentIndex * 33.3%; // Calcula o deslocamento
     carrosselInner.style.transform = `translateX(${offset}%)`; // Move o carrossel
 }
+//Centraliza o href ao clicar
+function centralizarSeção(event, id) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    
+    const elemento = document.getElementById(id);
+  
+    // Calcula a posição vertical que vai centralizar o elemento na tela
+    const posicaoElemento = elemento.offsetTop;
+    const alturaJanela = window.innerHeight;
+    const alturaElemento = elemento.offsetHeight;
+  
+    // Posição do topo para centralizar o elemento na tela
+    const posicaoCentralizada = posicaoElemento - (alturaJanela / 2) + (alturaElemento / 2);
+  
+    // Faz o scroll suave até a posição calculada
+    window.scrollTo({
+      top: posicaoCentralizada,
+      behavior: 'smooth'
+    });
+  }
