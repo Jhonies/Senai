9deb18662bf1b967b308d8f758035e85126f7c62
umtalderafa <rafacontato97@outlook.com>
2024-12-02 21:54:34 -0300

    commit-angular-erro

---
+++ Front-End/Gamemania-angular/gamemania-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component'; // Importando o AppComponent
+
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent // Declarando o AppComponent no módulo
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
