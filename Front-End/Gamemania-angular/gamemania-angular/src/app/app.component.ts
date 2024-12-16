9feb1b83071c92fb555fb6ee19c5b8e0db81125c
João Lopes <vlopes289@gmail.com>
2024-12-16 17:44:35 -0300

    commit angular

--- Front-End/Gamemania-angular/gamemania-angular/src/app/app.component.ts
+++ Front-End/Gamemania-angular/gamemania-angular/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
