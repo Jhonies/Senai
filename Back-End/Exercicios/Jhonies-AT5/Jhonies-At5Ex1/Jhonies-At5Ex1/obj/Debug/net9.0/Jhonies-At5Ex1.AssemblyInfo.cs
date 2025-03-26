32fada086181a7c9d61b3d85bccee5c351156c95
João Lopes <vlopes289@gmail.com>
2025-03-26 16:27:53 -0300

    At5-tratamento-exceptions

--- Back-End/Exercicios/Jhonies-AT5/Jhonies-At5Ex1/Jhonies-At5Ex1/obj/Debug/net9.0/Jhonies-At5Ex1.AssemblyInfo.cs
+++ Back-End/Exercicios/Jhonies-AT5/Jhonies-At5Ex1/Jhonies-At5Ex1/obj/Debug/net9.0/Jhonies-At5Ex1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jhonies-At5Ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db629f65aaf78bfef8ae66520a204ceaf72bb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8b9ca8acd336dd2320fa5e70f653ebd5f06603")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jhonies-At5Ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jhonies-At5Ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
